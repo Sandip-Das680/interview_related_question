6e44bf2f964f650ddd2de8b71aa489862de25f42
Sandip-Das680 <66658892+Sandip-Das680@users.noreply.github.com>
2025-12-30 20:00:22 +0530

    Non repeating char problem modified and increasing triplet problem added

--- Non_repeating_char.py
+++ Non_repeating_char.py
@@ -5,6 +5,19 @@ Output ='p'
 '''
 from collections import Counter
 
+def Non_repeating_char(word):
+
+    freq={}
+
+    for ch in word:
+        freq[ch]= freq.get(ch,0)+1
+    print(freq)
+    for ch in freq:
+        if freq[ch]==1:
+            print(ch)
+            break
+
+
 imput="programming"
 
 freq_ch= Counter(imput)
@@ -13,3 +26,5 @@ for ch in freq_ch:
     if freq_ch[ch]==1:
         print(ch)
         break
+Non_repeating_char(imput)
+
